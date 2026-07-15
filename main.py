9b3f25e0c40b41c2eb73e7a6c876f7daa0143c23
Ayaan01234 <ayaanqureshiq63@gmail.com>
2026-07-15 15:40:04 +0530

    Add summary of less commands to tatusq

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
+from sqlmodel import SQLModel
+from sqlalchemy.exc import OperationalError
+from database import engine
+from routers import cities, auth, rankings, ai, comparisons
 
-app = FastAPI()
-@app.get("/")
-def home ():
-    return {"message" : "server is running"}
+app = FastAPI(title="WhereToLive API")
+@app.on_event("startup")
+def on_startup():
+  
+  try:
+    SQLModel.metadata.create_all(engine)
+    print("Database Connected Successfully!")
+  except OperationalError as e:
+   print("Database Connection Failed:", e)
+
+
+app.include_router(cities.router)
+app.include_router(auth.router)
+app.include_router(rankings.router)
+app.include_router(ai.router)
+app.include_router(comparisons.router)
