c3c540a21b2ce1d5509a16613fef1da1c26840dc
Ayaan01234 <ayaanqureshiq63@gmail.com>
2026-07-17 12:26:48 +0530

    bugs fixes in seed

--- seed/seed_runner.py
+++ seed/seed_runner.py
@@ -1,34 +1,45 @@
 import csv
-from sqlmodel import Session, select
+from sqlmodel import Session, select, SQLModel
 from database import engine
-from models import city
 from models.city import City, CityMetric
+
+
 def run_seed():
-  with Session(engine) as session:
-    with open("app/seed/cities_seed.csv") as f:
-      reader = csv.DictReader(f)
-  for row in csv.readerr:
-    stmt = select(City).where(City.name == row["name"])
-    city = session.exec(stmt).first()
-  if not city:
-    city = City(
-    name=row["name"],
-    state=row["state"],
-    population=int(row["population"]),
- )      
-    session.add(city)
-    session.commit()
-    session.refresh(city)
-    metric = CityMetric(
-    city_id=city.id,
-    year=int(row["year"]),
-   cost_of_living_index=float(row["cost_index"]),
-   air_quality_index=float(row["air_index"]),
-   safety_score=float(row["safety_score"]),
-   healthcare_score=float(row["health_score"]),
- )
-  session.add(metric)
-  session.commit()
-  print("Seed complete!")
+    SQLModel.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        with open("seed/cities_seed.csv", newline="", encoding="utf-8") as f:
+            reader = csv.DictReader(f)
+
+            for row in reader:
+                stmt = select(City).where(City.name == row["name"])
+                city = session.exec(stmt).first()
+
+                if not city:
+                    city = City(
+                        name=row["name"],
+                        state=row["state"],
+                        population=int(row["population"]),
+                    )
+                    session.add(city)
+                    session.commit()
+                    session.refresh(city)
+
+                metric = CityMetric(
+                    city_id=city.id,
+                    year=int(row["year"]),
+                    cost_of_living_index=float(row["cost_index"]),
+                    air_quality_index=float(row["air_index"]),
+                    safety_score=float(row["safety_score"]),
+                    healthcare_score=float(row["health_score"]),
+                )
+
+                session.add(metric)
+
+            session.commit()
+
+    print("Seed complete!")
+
+
 if __name__ == "__main__":
- run_seed()
+    run_seed()
